2e3cdbc0bfc15c5006f8ef0730faa9153e23a9bf
Mehdi Achour <machour@gmail.com>
2019-04-13 23:24:27 +0100

    Update yiisoft/log namespace

--- src/debug/MongoDbPanel.php
+++ src/debug/MongoDbPanel.php
@@ -10,7 +10,7 @@ namespace yii\mongodb\debug;
 use Yii;
 use yii\debug\models\search\Db;
 use yii\debug\panels\DbPanel;
-use yii\log\Logger;
+use Yii\Log\Logger;
 
 /**
  * MongoDbPanel panel that collects and displays MongoDB queries performed.
