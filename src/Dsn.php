281bf8807bd15e4e98d950ce209c5043d7227e11
EAMoiseenko <eamoiseenko@beeline.ru>
2024-04-24 18:00:52 +0300

    Dropped all old code and added Dsn class for first implementation

---
+++ src/Dsn.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\MongoDb;
+
+/**
+ * Implement a Data Source Name (DSN) for MongoDb.
+ *
+ * @see https://www.php.net/manual/en/mongodb-driver-manager.construct.php
+ */
+final class Dsn
+{
+    public function __construct(
+        public ?string $uri,
+        public array $uriOptions = [],
+        public array $driverOptions = []
+    ) {}
+}
